568b52dfdafff59026d3deb9a1d552ab4f1d7b45
Hugo Bengtsson <hugo@capellium.se>
2022-04-26 18:36:33 +0200

    Cleaning up the code and changing function names

--- drone-freight-option.php
+++ drone-freight-option.php
@@ -9,148 +9,61 @@
 
 */
 
-
-/**
- * Check if WooCommerce is active
- */
-// if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-
-// 	function drone_shipping_method_init() {
-// 		if ( ! class_exists( 'WC_Drone_Shipping_Method' ) ) {
-// 			class WC_Drone_Shipping_Method extends WC_Shipping_Method {
-// 				/**
-// 				 * Constructor for your shipping class
-// 				 *
-// 				 * @access public
-// 				 * @return void
-// 				 */
-// 				public function __construct() {
-// 					$this->id                 = 'drone_shipping_method'; // Id for your shipping method. Should be uunique.
-// 					$this->method_title       = __( 'Drone shipment' );  // Title shown in admin
-// 					$this->method_description = __( 'Get your order delivered with a drone' ); // Description shown in admin
-
-// 					$this->enabled            = "yes"; // This can be added as an setting but for this example its forced enabled
-// 					$this->title              = "Drone Shipping"; // This can be added as an setting but for this example its forced.
-
-// 					$this->init();
-// 				}
-
-// 				/**
-// 				 * Init your settings
-// 				 *
-// 				 * @access public
-// 				 * @return void
-// 				 */
-// 				function init() {
-// 					// Load the settings API
-// 					$this->init_form_fields(); // This is part of the settings API. Override the method to add your own settings
-// 					$this->init_settings(); // This is part of the settings API. Loads settings you previously init.
-
-// 					// Save settings in admin if you have any defined
-// 					add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
-// 				}
-
-// 				/**
-// 				 * calculate_shipping function.
-// 				 *
-// 				 * @access public
-// 				 * @param mixed $package
-// 				 * @return void
-// 				 */
-// 				public function calculate_shipping( $package = array() ) {
-// 					$rate = array(
-// 						'label' => $this->title,
-// 						'cost' => '200,00',
-// 						'calc_tax' => 'per_item'
-// 					);
-
-// 					// Register the rate
-// 					$package->add_rate( $rate );
-// 				}
-// 			}
-// 		}
-// 	}
-
-// 	add_action( 'woocommerce_shipping_init', 'drone_shipping_method_init' );
-
-// 	function add_drone_shipping_method( $methods ) {
-// 		$methods['drone_shipping_method'] = 'WC_Drone_Shipping_Method';
-// 		return $methods;
-// 	}
-
-// 	add_filter( 'woocommerce_shipping_methods', 'add_drone_shipping_method' );
-// }
-
-
-
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 
-function your_shipping_method_init() {
-    if ( ! class_exists( 'WC_Your_Shipping_Method' ) ) {
-        class WC_Your_Shipping_Method extends WC_Shipping_Method {
-            /**
-             * Constructor for your shipping class
-             *
-             * @access public
-             * @return void
-             */
-            public function __construct() {
-                $this->id                 = 'drone_shipping_method'; // Id for your shipping method. Should be uunique.
-                $this->method_title       = __( 'Drone Shipment' );  // Title shown in admin
-                $this->method_description = __( 'Drone shipping to your house.' ); // Description shown in admin
+    function drone_method_init() {
+        if ( ! class_exists( 'WC_Drone_Shipping_Method' ) ) {
 
-                $this->enabled            = "yes"; // This can be added as an setting but for this example its forced enabled
-                $this->title              = "Drone Shipping"; // This can be added as an setting but for this example its forced.
+            // Drone Shipping class extending the WooCommerce shipment API.
+            class WC_Drone_Shipping_Method extends WC_Shipping_Method {
 
-                $this->init();
-            }
+                // Constructing the new class.
+                public function __construct() {
+                    $this->id                 = 'drone_shipping_method'; // ID for your shipping method. Should be uunique.
+                    $this->method_title       = __( 'Drone Shipment' );  // Title shown in admin
+                    $this->method_description = __( 'Drone shipping to your house.' ); // Description shown in admin
 
-            /**
-             * Init your settings
-             *
-             * @access public
-             * @return void
-             */
-            function init() {
-                // Load the settings API
-                $this->init_form_fields(); // This is part of the settings API. Override the method to add your own settings
-                $this->init_settings(); // This is part of the settings API. Loads settings you previously init.
+                    $this->enabled            = "yes";
+                    $this->title              = "Drone Shipping";
 
-                // Save settings in admin if you have any defined
-                add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
-            }
+                    $this->init();
+                }
 
-            /**
-             * calculate_shipping function.
-             *
-             * @access public
-             * @param mixed $package
-             * @return void
-             */
-            public function calculate_shipping( $package = array() ) {
-                $rate = array(
-                    'label' => $this->title,
-                    'cost' => '200.00',
-                    'calc_tax' => 'per_item'
-                );
 
-                // Register the rate
-                $this->add_rate( $rate );
+                function init() {
+                    // Load the settings API
+                    $this->init_form_fields();
+                    $this->init_settings();
+
+                    // Saves settings in admin if you have any defined
+                    add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
+                }
+
+                // Function for calculate the shipping cost.
+                public function calculate_shipping( $package = array() ) {
+                    $rate = array(
+                        'label' => $this->title,
+                        'cost' => '200.00',
+                        'calc_tax' => 'per_item'
+                    );
+    
+                    // Register the rate
+                    $this->add_rate( $rate );
+                }
             }
         }
     }
-}
-
-add_action( 'woocommerce_shipping_init', 'your_shipping_method_init' );
-
-function add_your_shipping_method( $methods ) {
-    $methods['your_shipping_method'] = 'WC_Your_Shipping_Method';
-    return $methods;
-}
-
-add_filter( 'woocommerce_shipping_methods', 'add_your_shipping_method' );
-}
-
+    
+    // Hook that is called after the plugins are loaded.
+    add_action( 'woocommerce_shipping_init', 'drone_method_init' );
+    
+    function add_drone_shipping_method( $methods ) {
+        $methods['your_shipping_method'] = 'WC_Drone_Shipping_Method';
+        return $methods;
+    }
+    
+    add_filter( 'woocommerce_shipping_methods', 'add_drone_shipping_method' );
+    }
 
 
 ?>
